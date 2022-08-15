d11a13e13061fcc6793102a362bd4bc3ab00845f
Jônatha Mendonça dos Santos <jonatha.mendonca@hotmail.com>
2022-08-15 14:27:20 -0300

    base da logica com matriz

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,192 +1,171 @@
 ﻿using System;
 
-namespace Teste
+namespace Teste2._0
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("tamanhoVetor");
 
-            // Network elemento = new Network();
+            Console.BackgroundColor = ConsoleColor.Blue;
 
-            //elemento.InserirTamanhoVetor();
+            Console.WriteLine("Digite a quantidade de elementos da rede: ");
+           
+            int tamanhoLinhasMatriz = int.Parse(Console.ReadLine());
 
+            int tamanhoColunasMatriz = tamanhoLinhasMatriz;
 
+            int[,] matriz = new int[tamanhoLinhasMatriz, tamanhoColunasMatriz];
 
-            // int tamanhoVetor = int.Parse(Console.ReadLine());
+            Console.Clear();
 
-            int tamanhoVetor = 5;
+            //ExibirNetwork();
+
             int opcao = 0;
-            //Console.WriteLine(tamanhoVetor);
 
-            //int[] numElementos[tamanhoVetor];
-
-            //preencher vetor
-            int[] elementos = new int[tamanhoVetor];
-            // int count = 1;
-            //int countj = 1;
-
-            /*
-            for (int i = 0; i < tamanhoVetor; i++)
-            {
-                for (int j = 0; j < tamanhoVetor; j++)
-                {
-
-                    elementos[i] = count;
-
-                    Console.WriteLine(elementos[i]);
-
-
-                    count++;
-                    countj++;
-
-                }
-
-            }
-            */
-
-
-            int[,] array2D = new int[5, 2] { { 1, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 }, { 5, 5 } };
-
-            //  Listar();
-            void Listar()
-            {
-                System.Console.Write(array2D[0, 0]);
-                System.Console.WriteLine(array2D[0, 1]);
-
-                System.Console.Write(array2D[1, 0]);
-                System.Console.WriteLine(array2D[1, 1]);
-
-                System.Console.Write(array2D[2, 0]);
-                System.Console.WriteLine(array2D[2, 1]);
-
-                System.Console.Write(array2D[3, 0]);
-                System.Console.WriteLine(array2D[3, 1]);
-
-                System.Console.Write(array2D[4, 0]);
-                System.Console.WriteLine(array2D[4, 1]);
-
-            }
-
-            // System.Console.WriteLine(array2D[1, 0]);
-            //  System.Console.WriteLine(array2D[1, 1]);
-            // System.Console.WriteLine(array2D[3, 0]);
-            // System.Console.WriteLine(array2D[1, 0]);
-
-            int elemento1;
-            int elemento2;
-
-
-            // Conectar();
-
-            void Conectar()
-            {
-                System.Console.Write(" digite o primeiro elemento: ");
-                elemento1 = int.Parse(Console.ReadLine());
-                System.Console.Write(" digite o segundo elemento: ");
-                elemento2 = int.Parse(Console.ReadLine());
-
-
-                System.Console.Write(elemento1);
-                System.Console.WriteLine(elemento2);
-
-
-
-                //ler a array2D
-
-
-                //procurar valor e se tem conenexao***************
-
-                //buscar cada elemento nas linhas na array2d
-                for (int i = 0; i < tamanhoVetor; i++)
-                {
-
-
-                    if (array2D[i, 1] == elemento1)
-                    {
-                        System.Console.WriteLine($"existe na posicao: linha {i} e coluna 0");
-                    }
-                    else
-                        System.Console.WriteLine($"nao existe conexao com nenhum outro apenas com ele mesmo!");
-
-
-                }
-
-                //inserir conexao na array 2d
-                array2D[4, 0] = elemento1;
-                array2D[4, 1] = elemento2;
-
-
-            }
-
-
-
+            ImprimirMenu();
 
             void ImprimirMenu()
             {
-
-                System.Console.WriteLine("1. Listar");
-                System.Console.WriteLine("2. Conectar");
-                //System.Console.WriteLine("3. Exibir menu");
-
-                System.Console.Write("Digite a opcao: ");
-
                 do
                 {
+                    Console.Clear();
+                    ExibirNetwork();
+
+                    Console.WriteLine("\nMenu:");
+                    Console.WriteLine("1. Conectar");
+                    Console.WriteLine("2. Consultar");
+                    Console.WriteLine("3. Ajuda");
+
+                    Console.Write("\nDigite a opcao: ");
+
                     opcao = int.Parse(Console.ReadLine());
-
-                    //   if (opcao == 1 || opcao == 2 || opcao == 3)
-                    //   {
-
 
                     switch (opcao)
                     {
                         case 1:
-                            Listar();
-                            break;
-                        case 2:
                             Conectar();
                             break;
+                        case 2:
+
+                            if (Consultar())
+                            {
+                                Console.WriteLine("O elemento está conectado");                           
+                            }
+                            else
+                            {
+                                Console.WriteLine("O elemento não está conectado");                            
+                            }
+                            Console.ReadLine();
+                            break;
                         case 3:
+                            Console.Write("Tem que ter um padrão de escolha e elemento e suas conexões! \n" +
+                                "Exemplo: O elemento de linha com conexões de colunas" +
+                                "\n ou elemento de coluna com conexões de linhas");
+                            Console.ReadLine();
+                            break;
+                        case 4:
+                            ExibirNetwork();
+                            break;
+                        default:
                             ImprimirMenu();
                             break;
 
-
-
                     }
-                    //   }
-                    //  else
-                    //  {
-                    //      System.Console.WriteLine("Opcao invalida!");
-
-                    //  }
+              
                 } while (opcao != 9);
             }
 
+            void ExibirNetwork()
+            {
+                ExibirTitulosColunas();
 
-            //menu sem parar
+                for (int linha = 0; linha < tamanhoLinhasMatriz; linha++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(linha + "|");
 
-            /*
+                    Console.ForegroundColor = ConsoleColor.White;
 
-                        do
+                    for (int coluna = 0; coluna < tamanhoColunasMatriz; coluna++)
+                    {                  
+                        Console.Write($"{matriz[linha, coluna]} "); // valor do elemento da matriz
+                    }
+
+                    Console.WriteLine(" ");
+                }
+            }
+
+            void ExibirTitulosColunas()
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                ///Console.WriteLine("COLUNAS");
+
+                for (int i = 0; i < tamanhoColunasMatriz; i++)
+                {
+                    if (i == 0)
+                        Console.Write("  ");
+
+
+                    Console.Write($"{i} ");
+                }
+
+                Console.Write("\n");
+
+                for (int i = 0; i < tamanhoColunasMatriz; i++)
+                {
+                    Console.Write("---");
+                }
+                Console.Write("\n");
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
+            //conectar                  
+            void Conectar()
+            {
+                int elemento1;
+                int elemento2;
+
+                System.Console.Write("Digite o primeiro elemento(linha) que vc quer fazer conecxao: ");
+                elemento1 = int.Parse(Console.ReadLine());
+                System.Console.Write("Digite o segundo elemento(coluna) que voce quer conectar com ele: ");
+                elemento2 = int.Parse(Console.ReadLine());
+
+                ExibirTitulosColunas();
+
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    matriz[elemento1, elemento2] = 1;
+
+            }
+
+            //consultar
+            bool Consultar()
+            {
+                int elemento1;
+                int elemento2;
+
+                System.Console.Write("Digite o primeiro elemento(linha) que você quer consutar a conecxao: ");
+                elemento1 = int.Parse(Console.ReadLine());
+                System.Console.Write("Digite o segundo elemento(coluna) que voce quer consutar a conecxao com ele: ");
+                elemento2 = int.Parse(Console.ReadLine());
+       
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.ForegroundColor = ConsoleColor.White;
+               
+                        if (matriz[elemento1, elemento2] == 1)
                         {
-                            Imprimirmenu();
-
-                                        System.Console.Write(" digite a opcao ");
-                                        opcao = int.Parse(Console.ReadLine());
-
-                        } while (opcao != 9);
-            */
-
-            ImprimirMenu();
-
-
-
-
-
-
-
+                            return true;
+                        }
+                        else
+               
+                return false;
+            }
 
         }
     }
